45124951a36826631ae7eedd439c93a4100993da
Zenel Kazushi <zenelk@gmail.com>
2025-11-12 15:36:44 -0700

    Add missing sub-plugins to git plugin

--- git/git-plugin.zsh
+++ git/git-plugin.zsh
@@ -3,7 +3,9 @@ ZK_GIT_SPECIAL_BRANCHES=("main" "master")
 
 source "${0:a:h}/aliases.zsh"
 source "${0:a:h}/config.zsh"
+source "${0:a:h}/fz.zsh"
 source "${0:a:h}/gbnr.zsh"
 source "${0:a:h}/gcb.zsh"
 source "${0:a:h}/gdb.zsh"
 source "${0:a:h}/grc.zsh"
+source "${0:a:h}/sr.zsh"
