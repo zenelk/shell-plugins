0a82618a7fdb4f2d1fd5ba9f081ea06ef8dd3f57
Zenel Kazushi <zenelk@gmail.com>
2026-04-20 16:31:33 -0600

    Clean up function_redefine comment

--- init/init-plugin.zsh
+++ init/init-plugin.zsh
@@ -1,6 +1,17 @@
+# ---
+# Function Redefinition
+# ---
+#
 # Allows for functions to be reloaded when used like so:
 #   function_redefine foo
 #   function foo() { ... }
+#
+# All other plugins use this pattern to allow for functions to be overridden by other plugins if needed.
+# This is especially useful for development, as it allows for functions to be reloaded without needing
+# to restart the shell.
+#
+# NOTE: `function_redefine` itself cannot be reloaded and will need a shell restart to pick up changes.
+
 function function_redefine() {
   function function_exists() {
     declare -f "${1}" > /dev/null
