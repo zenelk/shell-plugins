db33802b4659bcc6df266b85c140a91a09b92b06
Zenel Kazushi <zenelk@gmail.com>
2026-04-23 15:42:42 -0600

    Update gdb to use brace var expansion

--- git/gdb.zsh
+++ git/gdb.zsh
@@ -1,10 +1,10 @@
 # ZTODO: Should handle multiple remotes.
 function_redefine gdb
 function gdb() {
-  if [ -z "$1" ]; then
+  if [ -z "${1}" ]; then
     zk_log_error "Branch name is required."
     return
   fi
-  git branch -D "$1"
-  git push origin ":$1"
+  git branch -D "${1}"
+  git push origin ":${1}"
 }
